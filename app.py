f1388c6830fb0306df405bf65fe78e4d301ed265
brackcarour <99796801+brackcarour@users.noreply.github.com>
2022-02-23 15:47:53 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+from flask import Flask
+
+
+# In[19]:
+
+
+app = Flask(__name__)
+
+
+# In[20]:
+
+
+from flask import request, render_template
+from keras.models import load_model
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        income = request.form.get("income")
+        age = request.form.get("age")
+        loan = request.form.get("loan")
+        print(income, age, loan)
+        model = load_model("default_model")
+        pred = model.predict([[float(income), float(age), float(loan)]])
+        s = "The predicted default score is " + str(pred)
+        return(render_template("index.html", result = s))
+    else:
+        return(render_template("index.html", result = "2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=int("80"))
+
+
+# In[ ]:
+
+
+
+
